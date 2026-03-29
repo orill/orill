469454d1806410ec6e19b667a55c4213d48e4ec8
ORILL <39681553+orill@users.noreply.github.com>
2026-03-29 10:30:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,7 @@ pygments_style = 'default'
 
 
 
-html_theme = "agogo"
+html_theme = "nature"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
