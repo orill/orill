55a024be61ad3b998cbb91a542f8cfab304cfc50
ORILL <39681553+orill@users.noreply.github.com>
2026-03-29 10:19:12 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ pygments_style = 'default'
 #]
 
 
-html_theme = "bizstyle"
+html_theme = "sphinxdoc"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
